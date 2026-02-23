25a1564fb8911bc07702a25ee820850e14039be3
iceezt <cachebea@gmail.com>
2026-02-23 19:23:17 +0300

    Create internal_offsets.hpp

---
+++ offsets/internal_offsets.hpp
@@ -0,0 +1,32 @@
+#pragma once
+/* =============================================================
+                       leafy dumper                            
+ -------------------------------------------------------------
+  Roblox Version  : version-df7528517c6849f7
+  Dumper Version  : v1.2.1ar (alpha rewrite)
+  Dumped At       : 19:22 23/02/2026 (GMT)
+  Total Offsets   : 13
+ -------------------------------------------------------------
+ =============================================================
+*/
+
+#include <cstdint>
+#include <string>
+
+namespace Internal {
+    inline std::string ClientVersion = "version-df7528517c6849f7";
+
+    inline constexpr uintptr_t appdata_info = 0x7e11ff0;
+    inline constexpr uintptr_t firemousehoverenter = 0x9ece90;
+    inline constexpr uintptr_t firetouchinterest = 0x2593290;
+    inline constexpr uintptr_t getglobalstate = 0x1955d60;
+    inline constexpr uintptr_t getscheduler = 0x1a6cfd0;
+    inline constexpr uintptr_t ktable = 0x774bf70;
+    inline constexpr uintptr_t lock_violation_crash = 0x77c00f8;
+    inline constexpr uintptr_t luad_throw = 0x3a2c400;
+    inline constexpr uintptr_t opcode_lookup_table = 0x9873cd5;
+    inline constexpr uintptr_t physics_sender_max_bandwidth = 0x69f6e10;
+    inline constexpr uintptr_t raw_scheduler = 0x7ef3c48;
+    inline constexpr uintptr_t rbx_deserialize = 0x4e04110;
+    inline constexpr uintptr_t task_scheduler_target_fps = 0x774bef0;
+}
